d692df70c53e1ddbc5aea226db3c15a9fcd39989
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-13 19:57:57 -0800

    successfully imported and displayed logo!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+          <div className="navbar-left">
+              <img src={logo} alt="" className="" />
+        </div>
+        <div className="navbar-right"></div>
+    </div>
   )
 }
 
