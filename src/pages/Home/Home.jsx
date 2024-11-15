978c49e16d69b15edc33b737b88ecddbab177ae4
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-14 20:55:56 -0800

    buttons and icons added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -13,6 +15,10 @@ const Home = () => {
               <div className="hero-caption">
                   <img src={hero_title} alt="" className="caption-img" />
                   <p className="">Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                  <div className="hero-btns">
+                      <button className=""><img src={play_icon} alt="" className="" />Play</button>
+                      <button className=""><img src={info_icon} alt="" className="" />Play</button>
+                  </div>
               </div>
           </div>
     </div>
