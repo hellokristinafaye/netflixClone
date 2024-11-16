5de321e9d0f7f20282eda224f0020ed449bc3198
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-15 21:42:13 -0800

    log in page content added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,8 +7,19 @@ const Login = () => {
       <div className='login'>
           <img src={logo} alt="" className="login-logo" />
           <div className="login-form">
-              <h1 className="">Sign In</h1>
-              <form action="" className=""></form>
+              <h1 className="">Sign Up</h1>
+              <form className="">
+                  <input type="text" placeholder="Your Name" className="" />
+                  <input type="email" placeholder="Email" className="" />
+                  <input type="password" placeholder="Password" className="" />
+                  <button className="">Sign Up</button>
+                  <div className="form-help"></div>
+                  <div className="remember">
+                      <input type="checkbox" className="" />
+                      <label htmlFor="" className="">Remember Me</label>
+                  </div>
+                  <p className="">Need Help?</p>
+                </form>
           </div>
     </div>
   )
