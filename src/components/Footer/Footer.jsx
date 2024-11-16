5265aff0b1a9a224a774047a02672a6c50ea12bf
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-15 21:05:43 -0800

    footer added to Home. also social images added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png';
+import twitter_icon from '../../assets/twitter_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import facebook_icon from '../../assets/facebook_icon.png';
+
 
 const Footer = () => {
   return (
-    <div className='footer'></div>
+    <div className='footer'>
+          <div className="footer-icons">
+              <img src={facebook_icon} alt="" className="" />
+              <img src={instagram_icon} alt="" className="" />
+              <img src={twitter_icon} alt="" className="" />
+              <img src={youtube_icon} alt="" className="" />
+          </div>
+    </div>
   )
 }
 
