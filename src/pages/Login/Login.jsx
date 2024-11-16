c0d90aec82e3fa6293e23561ec601259d3ec2218
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-15 21:35:36 -0800

    lil debugging - the react-router-dom didn't install properly for me? I had to make adjustments to make it match the tutorial and now it works

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
-    <div className='login'></div>
+      <div className='login'>
+          <img src={logo} alt="" className="login-logo" />
+          <div className="login-form">
+              <h1 className="">Sign In</h1>
+              <form action="" className=""></form>
+          </div>
+    </div>
   )
 }
 
