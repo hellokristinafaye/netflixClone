ea03eefec8ae16c39c084a5f917c4cb693cda4ca
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-18 17:25:13 -0800

    data is finally loading from TMDB. .the console.log line was interfering?? Ok..)

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
@@ -6,7 +6,18 @@ import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
   
+    const [apiData, setApiData] = useState([]);
     const cardsRef = useRef();
+
+    const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWM2YWJhY2RmNjcwNGRmYzFkNDNkNGIzYzc4MTc2ZiIsIm5iZiI6MTczMTkwMjAxNC4zMTE1MjE4LCJzdWIiOiI2NzNhYjg3NGY3NDFlYjA0MjhiNjFmMjYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HnikSMVXmScqffVt079FGhXMCtxyhFvRnyoFuvby7xg'
+  }
+};
+
+
 
     const handleWheel = (event) => {
      event.preventDefault();
@@ -15,7 +26,14 @@ const TitleCards = ({title, category}) => {
 
     useEffect(() => {
     // the wheel stuff seems depricated, but I do want this useEffect
-    // cardsRef.current.addEventListener('wheel', handleWheel)
+        // cardsRef.current.addEventListener('wheel', handleWheel)
+        
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+         .then(response => response.json())
+        // .then(response => console.log(response))
+        .then(response => setApiData(response.results))
+            .catch(err => console.error(err));
+        
 }, [])
 
   
@@ -23,10 +41,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
           <h2 className="">{title?title:"Popular on Netflix"}</h2>
           <div className="card-list" ref={cardsRef}>
-              {cards_data.map((card, index) => {
+              {apiData.map((card, index) => {
                   return <div className="card" key={index}>
-                      <img src={card.image} alt="" className="" />
-                      <p className="">{card.name}</p>
+                      <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" className="" />
+                      <p className="">{card.original_title}</p>
                   </div>
               })}
           </div>
