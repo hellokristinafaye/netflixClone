d659d12806034a01c5d1a6e490383adf99fa45ed
Kristina Faye <hellokristinafaye@gmail.com>
2024-11-18 17:56:48 -0800

    added/routed the Player screen! needs a little more work but this is a start

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
-    <div className='player'></div>
+      <div className='player'>
+          <img src={back_arrow_icon} alt="" className="" />
+          <iframe width='90%' height='90%' src='https://www.youtube.com/embed/jfWa-GD0szU' className="" title='trailer' frameBorder='0' allowFullScreen ></iframe>
+          <div className="player-info">
+              <p className="">Published Date</p>
+              <p className="">Name</p>
+              <p className="">Type</p>
+          </div>
+    </div>
   )
 }
 
