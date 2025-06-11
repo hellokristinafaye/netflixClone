7c28c6f14fda4b6d64db2f88724e280fd44a4b68
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-11 16:32:35 -0700

    banner updated and it looks GREAT

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import TitleCards from '../../components/TitleCards/TitleCards'
 import Footer from '../../components/Footer/Footer'
 // assets. provided by Great Stack
 import hero_banner from '../../assets/hero_banner.jpg'
+import SinnersBanner from "../../assets/SinnersBanner.png";
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
@@ -47,7 +48,7 @@ const Home = (index) => {
     <div className="home">
       <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className="banner-img" />
+        <img src={SinnersBanner} alt="" className="banner-img" />
         <div className="hero-caption">
           <img src={hero_title} alt="" className="caption-img" />
           <p className="">
@@ -56,13 +57,7 @@ const Home = (index) => {
             immortal enemy.
           </p>
           <div className="hero-btns">
-        
-
-            <Link
-              to={`/player/${apiData.id}`}
-              className="card"
-              key={index}
-            >
+            <Link to={`/player/${apiData.id}`} className="card" key={index}>
               <button className="btn">
                 <img src={play_icon} alt="" className="" />
                 Play
