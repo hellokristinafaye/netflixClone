b8daca8ebbac498e0778ad29913785ab34fc7060
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-11 14:11:52 -0700

    Link tag added to Play button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,33 +11,46 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import { Link } from 'react-router-dom'
 
 // so this is a function that returns JSX.  And then we export the function to the App file, which then displays code depending on which route is used in the URL 
 const Home = () => {
   return (
-      <div className='home'>
-          <Navbar />
-          <div className="hero">
-              <img src={hero_banner} alt="" className="banner-img" />
-              <div className="hero-caption">
-                  <img src={hero_title} alt="" className="caption-img" />
-                  <p className="">Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
-                  <div className="hero-btns">
-                      <button className="btn"><img src={play_icon} alt="" className="" />Play</button>
-                      <button className="btn dark-btn"><img src={info_icon} alt="" className="" />More Info</button>
-                  </div>
-                  <TitleCards />
-              </div>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p className="">
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <Link to={`/player/`} className="card" >
+              <button className="btn">
+                <img src={play_icon} alt="" className="" />
+                Play
+              </button>
+            </Link>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" className="" />
+              More Info
+            </button>
           </div>
-          <div className="more-cards">
-              <TitleCards title={ "Blockbuster Movies"} category={'top_rated'} />
-             <TitleCards title={ "Only on Netflix"} category={'popular'}/>
-             <TitleCards title={ "Upcoming"} category={'upcoming'}/>
-             <TitleCards title={ "Top Pics for You"} category={'now_playing'}/>
-          </div>
-          <Footer />
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
+        <TitleCards title={"Only on Netflix"} category={"popular"} />
+        <TitleCards title={"Upcoming"} category={"upcoming"} />
+        <TitleCards title={"Top Pics for You"} category={"now_playing"} />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Home
